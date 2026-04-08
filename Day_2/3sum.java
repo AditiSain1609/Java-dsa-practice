9dfb8c0f87a4d5e54131a474dffc9e8da980e76c
Aditi Saini <aditisaini519@gmail.com>
2026-04-08 12:06:40 +0530

    3

---
+++ Day_2/3sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+      List<List<Integer>> la = new ArrayList<>(); 
+      Arrays.sort(nums); 
+      for(int i=0;i<nums.length;i++){
+        if(i>0 && nums[i]==nums[i-1]) 
+        continue;
+        int left = i+1;
+        int right = nums.length-1;
+        while(left<right){
+            int sum = nums[i]+nums[left] +nums[right];
+            if(sum==0){
+                List<Integer> temp = new ArrayList<>();
+                temp.add(nums[i]);
+                temp.add(nums[left]);
+                temp.add(nums[right]);
+                left++;
+                right --;
+                while(left<right && nums[left]==nums[left-1])
+                left++;
+                while(left<right && nums[right] ==nums[right+1])
+                right--;
+                 la.add(temp);
+            }
+            else if(sum<0){
+                left ++;
+            }
+            else {
+                right--;
+            }
+        }
+       
+       
+      }
+      return la;
+    }
+}
